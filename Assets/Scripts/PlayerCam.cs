3e33a21e2eaae9fe7d7e6e0c020883a08cd7f142
Superlllas2 <64853531+Superlllas2@users.noreply.github.com>
2024-09-10 09:14:41 +0200

    File management

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float senseX;
+    public float senseY;
+
+    public Transform orientation;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        var mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senseX;
+        var mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senseY;
+
+        yRotation -= mouseX;
+        xRotation = Math.Clamp(xRotation, -90f, 90f);
+        
+        
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        
+    }
+}
